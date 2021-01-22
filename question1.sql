71063597eb047c0f711e7900e8e3c082f9d808a0
Samantha Hering <samanthahering@yahoo.com>
2021-01-22 18:42:32 -0500

    sql update

--- question1.sql
+++ question1.sql
@@ -1,3 +1,6 @@
-SELECT govt_debt/gdp_per_capita FROM countries
-ORDER BY govt_debt/gdp_per_capita DESC
-LIMIT 3;
+SELECT code , avg(govt_debt/gdp_per_capita) FROM countries
+WHERE year > 2017
+GROUP BY code
+HAVING MIN (gdp_per_capita) > 40000
+ORDER BY 2 DESC
+LIMIT 3
